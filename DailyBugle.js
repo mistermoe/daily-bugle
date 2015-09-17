e7571df388846c9d950a6bcecd8770e7d6faeadc
mistermoe <moeez@utexas.edu>
2015-09-17 01:32:13 -0500

    a single DailyBugle instance can now subscribe to multiple channels.

--- DailyBugle.js
+++ DailyBugle.js
@@ -1,14 +1,27 @@
 var DailyBugle = function() {
-	this.port = chrome.runtime.connect();
+  this.port = chrome.runtime.connect();
+  this.channels = {};
+  this.port.onMessage.addListener(function(response){
+    var channelFuncs = this.channels[response.channel];
+    for (var i = 0; i < channelFuncs.length; i += 1) {
+      (function(func){
+        func(response.args);
+      })(channelFuncs[i]);
+    }
+  }.bind(this));
 
-	this.on = function(channel, func) {
-		this.port.postMessage({subscribeTo: channel});
-		
-		this.port.onMessage.addListener(func);
-	}
+  this.on = function(channel, func) {
+    this.port.postMessage({subscribeTo: channel});
+    if (this.channels[channel]) {
+      this.channels[channel].push(func);
+    }
+    else {
+      this.channels[channel] = [func];
+    }
+  }
 
-	this.publish = function(channel, args, opts) {
-		this.port.postMessage({publishTo: channel, args: args, opts: opts});
-	}
+  this.publish = function(channel, args, opts) {
+    this.port.postMessage({publishTo: channel, args: args, opts: opts});
+  }
 
 }
--- PrintingPress.js
+++ PrintingPress.js
@@ -1,108 +1,99 @@
 var PrintingPress = {
+  // all :  connected (from content-scripts) ports separated by the events they're subscribed to
+  connections: {},
+  // backgroundChannels : all the channels the background script is subcribed to along with each channel's respective listeners
+  backgroundChannels: {},
     
-    // all :  connected (from content-scripts) ports separated by the events they're subscribed to
-    connections: {},
-    // backgroundChannels : all the channels the background script is subcribed to along with each channel's respective listeners
-    backgroundChannels: {},
-    
-    // print : dispatcher
-    print: function() {
-
-        chrome.runtime.onConnect.addListener(function(port){
-            console.log("(PrintingPress.js) client connected");
-
-                
-            port.onMessage.addListener(function(msg){
-                console.log(msg);
-                if (msg.opts && msg.opts.background) {
-                    if (PrintingPress.backgroundChannels[msg.publishTo]) {
-                        for (var i = 0; i < PrintingPress.backgroundChannels[msg.publishTo].length; i += 1) {
-                            if (msg.args) {
-
-                                PrintingPress.backgroundChannels[msg.publishTo][i](msg.args);
-                            }
-                            else {
-                                PrintingPress.backgroundChannels[msg.publishTo][i]();
-                            }
-                        }
-                    }
-                }
-                
-                else if (msg.subscribeTo) {
-                    console.log("(PrintingPress.js) client subscribed to " + msg.subscribeTo + ".");
-                    if (PrintingPress.connections[msg.subscribeTo]) {
-                        PrintingPress.connections[msg.subscribeTo].push(port);
-                    }
-                    else {
-                        PrintingPress.connections[msg.subscribeTo] = [port];
-                    }
-                }
-                else if (msg.publishTo) {
-                    console.log("(PrintingPress.js) publishing message to " + msg.publishTo);
-                    var 
-                        subscribers = PrintingPress.connections[msg.publishTo],
-                        subscriber;
-
-                    for (var i = 0; i < subscribers.length; i += 1) {
-                        (function(subscriber){
-                            if (!subscriber) {
-                                return;
-                            }
-                            if (msg.args) {
-                                subscriber.postMessage(msg.args);
-                            }
-                            else {
-                                subscriber.postMessage(null);
-                            }
-                        })(subscribers[i]);
-                    }
-                }
-            });
-        });
-    },
-    // on : background script listener
-    on: function(channel, func) {
-        if (PrintingPress.backgroundChannels[channel]) {
-            PrintingPress.backgroundChannels[channel].push(func)
+  // print : dispatcher
+  print: function() {
+    chrome.runtime.onConnect.addListener(function(port){
+      port.onMessage.addListener(function(msg){
+        if (msg.opts && msg.opts.background) {
+          if (PrintingPress.backgroundChannels[msg.publishTo]) {
+            for (var i = 0; i < PrintingPress.backgroundChannels[msg.publishTo].length; i += 1) {
+              if (msg.args) {
+                PrintingPress.backgroundChannels[msg.publishTo][i](msg.args);
+              }
+              else {
+                PrintingPress.backgroundChannels[msg.publishTo][i]();
+              }
+            }
+          }
         }
-        else {
-            PrintingPress.backgroundChannels[channel] = [func];
+
+        else if (msg.subscribeTo) {
+          console.log("(PrintingPress.js) client subscribed to " + msg.subscribeTo + ".");;
+          if (PrintingPress.connections[msg.subscribeTo]) {
+            PrintingPress.connections[msg.subscribeTo].push(port);
+          }
+          else {
+            PrintingPress.connections[msg.subscribeTo] = [port];
+          }
         }
-    },
 
-    // unsubscribeAll : unsubscribes all listeners
-    unsubscribeAll: function() {
-        this.connections = {};
-        this.backgroundChannels = {};
-    },
-
-    unsubscribe: function(opts) {
-        if (!opts)
-            return;
-        if (opts.tabChannels)
-            this.connections = {};
-        if (opts.backgroundChannels)
-            this.backgroundChannels = {};
-    },
-
-    publish: function(channel, args, opts) {
-        var 
-            subscribers = this.connections[channel],
+        else if (msg.publishTo) {
+          console.log("(PrintingPress.js) publishing message to " + msg.publishTo);
+          var 
+            subscribers = PrintingPress.connections[msg.publishTo],
             subscriber;
 
-        for (var i = 0; i < subscribers.length; i += 1) {
+          for (var i = 0; i < subscribers.length; i += 1) {
             (function(subscriber){
-                if ( !subscriber ) {
-                    return;
-                }
-
-                if (args) {
-                    subscriber.postMessage(args);
-                }
-                else {
-                    subscriber.postMessage(null);
-                }
+              if (!subscriber) {
+                return;
+              }
+              var response = {
+                'channel': msg.publishTo,
+                'args': msg.args
+              }
+              subscriber.postMessage(response);
             })(subscribers[i]);
+          }
         }
+      });
+    });
+  },
+  // on : background script listener
+  on: function(channel, func) {
+    if (PrintingPress.backgroundChannels[channel]) {
+      PrintingPress.backgroundChannels[channel].push(func)
     }
+    else {
+      PrintingPress.backgroundChannels[channel] = [func];
+    }
+  },
+
+  // unsubscribeAll : unsubscribes all listeners
+  unsubscribeAll: function() {
+    this.connections = {};
+    this.backgroundChannels = {};
+  },
+
+  unsubscribe: function(opts) {
+    if (!opts)
+      return;
+    if (opts.tabChannels)
+      this.connections = {};
+    if (opts.backgroundChannels)
+      this.backgroundChannels = {};
+  },
+
+  publish: function(channel, args, opts) {
+    var 
+      subscribers = this.connections[channel],
+      subscriber;
+
+    for (var i = 0; i < subscribers.length; i += 1) {
+      (function(subscriber, channel, args){
+        if ( !subscriber ) {
+          return;
+        }
+        var response = {
+          'channel' : channel,
+          'args': args
+        }
+        subscriber.postMessage(response);
+      })(subscribers[i], channel, args);
+    }
+  }
 }
